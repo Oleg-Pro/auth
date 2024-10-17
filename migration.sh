c80ee85aec23ca07cfb1ab9073d7ebaf96f1a33b
Oleg Proshin <opp20071980@gmail.com>
2024-10-17 13:58:54 +0300

    correct migrator host

--- migration.sh
+++ migration.sh
@@ -2,6 +2,6 @@
 source .env
 
 export DB_HOST=${DB_HOST:-pg_auth}  # Использует "pg_auth" локально, но принимает значение из окружения, если оно задано
-export MIGRATION_DSN="host=pg_auth port=5432 dbname=$PG_DATABASE_NAME user=$PG_USER password=$PG_PASSWORD sslmode=disable"
+export MIGRATION_DSN="host=$DB_HOST port=5432 dbname=$PG_DATABASE_NAME user=$PG_USER password=$PG_PASSWORD sslmode=disable"
 
 sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
