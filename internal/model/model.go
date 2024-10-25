888f0ddacb018f2036007c014af9c51c32673d5d
Oleg Proshin <opp20071980@gmail.com>
2024-10-25 11:13:40 +0300

    removed commented code

--- internal/model/model.go
+++ internal/model/model.go
@@ -8,11 +8,6 @@ import (
 // Role user role
 type Role int32
 
-/*const (
-	Role_USER  Role = 1
-	Role_ADMIN Role = 2
-)*/
-
 // UserInfo info about user
 type UserInfo struct {
 	Name        string
